ae4674b0936b5eeaafad54837a8021efbf8ee4f6
move style to head <timmortal44@yahoo.com>
2022-06-05 23:40:19 +0100

    select reload button

--- app.js
+++ app.js
@@ -6,3 +6,5 @@ const logsLeft = document.querySelectorAll('.log-left')
 const logsRight = document.querySelectorAll('.log-right')
 const carsLeft = document.querySelectorAll('.car-left')
 const carsRight = document.querySelectorAll('.car-right')
+
+const reloadButton = document.querySelector('#reload')
